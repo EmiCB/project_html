bc77a414cd9e55e7ce9a39d9205cf3123a7d1429
Emi Brown <EmiCB@users.noreply.github.com>
2016-04-19 10:38:52 -0500

    Initial Commit

---
+++ project_html/Assets/name.js
@@ -0,0 +1,23 @@
+var myName = "EmiCB";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue, purple];
+
+var ratio = .5;
+
+drawName(myName, letterColors);
+
+if(10 < 3)
+{
+    bubbleShape = 'heart';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+
+bounceBubbles();
